11063b334f8a0f7cc4ff36211bc4491e72ae9250
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2024-11-03 17:04:03 +0000

    feat-explore: dummy data for explore page

--- src/features/explorePage/ExplorePage.tsx
+++ src/features/explorePage/ExplorePage.tsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
 
+import { USE_DUMMY } from '../../app/config';
 import { TickerCard } from '../cards/components/TickerCard';
+import DummyData from '../searchBar/files/dummyTickerSearchResults.json';
 import SearchBar from '../searchBar/SearchBar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './explore-page.css';
 
+type TickerResponseItem = {
+  symbol: string;
+  name: string;
+  region: string;
+  currency: string;
+  type: string;
+};
+
 export const ExplorePage = () => {
-  const [serchTerm, setSearchTerm] = useState({ value: '' });
+  const [searchTerm, setSearchTerm] = useState({ value: '' });
+  const [searchResponse, setSearchResponse] = useState<TickerResponseItem[]>([]);
+
   return (
     <div className="page-container-explore">
       <h5>Search Bar</h5>
       <SearchBar type="text" onChange={(o) => setSearchTerm(o)} placeholder="Search ticker values..." />
-      <TickerCard symbol="QQQ" name="" region="UK" currency="GDP" type="ETD" />
+      {USE_DUMMY ? (
+        <>
+          <div style={{ display: 'flex', maxWidth: 1500, flexWrap: 'wrap', alignItems: 'center' }}>
+            {DummyData.bestMatches.map((item: TickerResponseItem, index: number) => {
+              return (
+                <div key={index}>
+                  <TickerCard symbol={item.symbol} name={item.name} region={item.region} currency={item.currency} type={item.type} />
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
